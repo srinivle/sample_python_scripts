1f74cbfa351a420951b2f03ad64a1093e04d9b58
srinivle <pleelasrinivas85@gmail.com>
2024-04-18 00:50:28 +0530

    updated the file with new codes

--- sample.py
+++ sample.py
@@ -1,1 +1,8 @@
+import os
+ 
+print('Absolute path of file:     ', 
+      os.path.abspath('instance-status.csv'))
+print('Absolute directoryname: ', 
+      os.path.dirname(os.path.abspath(__file__)))
+
 print("Hello World")
