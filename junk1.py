94ad1a13767c50d9b3328372024d8c963333e6a1
srinivle <pleelasrinivas85@gmail.com>
2024-04-19 01:28:28 +0530

    new codes are added

--- junk1.py
+++ junk1.py
@@ -1,28 +1,79 @@
+import boto3
+import pandas as pd
+from pprint import pprint
+from io import StringIO
+
+#Opening the Boto3 Session.
+session = boto3.Session(profile_name='dev', region_name='us-east-1')
+client1 = session.client('s3')
+client2 = session.client('ec2')
+client3 = session.resource('ec2')
+'''
+response1 = client2.describe_volumes(
+                        VolumeIds=[
+                        'vol-00c1c38e29202cb7a',
+                        'vol-018cce0c1a15e2195',
+                        'vol-026af86250afff292',
+                    ],
+                )
+
+pprint(response1)
+'''
+response2 = client2.describe_snapshots(
+    Filters=[
+        {
+            'Name': 'tag:Env',
+            'Values': [
+                'Dev',
+            ]
+        },
+    ],
+)
+
+p = response2
+pprint(p)
+c = []
+for each in range(0,len(response2['Snapshots'])):
+    b = []
+    b.append(response2['Snapshots'][each]['SnapshotId'])
+    b.append(response2['Snapshots'][each]['StartTime'])
+    b.append(response2['Snapshots'][each]['State'])
+    b.append(response2['Snapshots'][each]['StorageTier'])
+    b.append(response2['Snapshots'][each]['VolumeId'])
+    b.append(response2['Snapshots'][each]['VolumeSize'])
+    b.append(response2['Snapshots'][each]['Tags'][0]['Key'])
+    b.append(response2['Snapshots'][each]['Tags'][0]['Value'])
+    c.append(b)
+
+pprint(c)
+
+'''
 from pprint import pprint
 
 def list_in_list(b):
         outside_list = []
         for each in range(0,b):
             inside_list = []      
-            '''
+            
             response1 = client2.describe_instances(
                                 InstanceIds=[
                                     df['instance-id'].values[each],
                                 ],
                                 )
-            '''
+            
             inside_list.append(each)
             inside_list.append(each+1)
             inside_list.append(each+2)
-            '''
+            
             inside_list.append(response1['Reservations'][0]['Instances'][0]['InstanceType'])    
             inside_list.append(response1['Reservations'][0]['Instances'][0]['PrivateIpAddress'])
             inside_list.append(response1['Reservations'][0]['Instances'][0]['RootDeviceType'])
             inside_list.append(response1['Reservations'][0]['Instances'][0]['State']['Name'])
-            '''
+            
             outside_list.append(inside_list)
         return outside_list
 
 a = list_in_list(5)
 
 pprint(a)
+'''
---
+++ samplevolfunc.py
@@ -0,0 +1,186 @@
+import boto3
+import pandas as pd
+from pprint import pprint
+from io import StringIO
+from datetime import datetime, date
+from dateutil.tz import *
+from dateutil import relativedelta
+
+#Opening the Boto3 Session.
+session = boto3.Session(profile_name='dev', region_name='us-east-1')
+client1 = session.client('s3')
+client2 = session.client('ec2')
+
+# Reading the Object
+response = client1.get_object(Bucket='sample88563', Key='list-of-volumes.csv')
+data = response['Body'].read().decode('utf-8')
+df = pd.read_csv(StringIO(data))
+pprint(df)
+
+# Total number of Objects in CSV File
+b = len(df)
+pprint(b)
+
+def create_tag_vol(tagvol):
+    # Creation of Tags for Volumes
+    for each in range(0,b):
+        response1 = client2.create_tags(                    
+                        Resources=[
+                            df['volume-id'].values[each],
+                        ],
+                        Tags=[
+                            {
+                                'Key': 'Env',
+                                'Value': 'Dev'
+                            },
+                        ]
+                    )
+    return response1
+
+def desc_tag_vol(descVol):
+    # Describe Tags for List of Volumes
+
+    for each1 in range(0,b):
+        response2 = client2.describe_tags(
+            Filters=[
+            {
+                'Name': 'tag:Env',
+                'Values': [
+                    'Dev',
+                ]
+            },
+        ],
+    )
+        
+    return response2
+
+# Describe Volumes for List of Volumes
+def list_in_lists(b):
+
+    outside_list = []
+    for each2 in range(0,b):
+        inside_list = []             
+        response3 = client2.describe_volumes(
+                        VolumeIds=[
+                        df['volume-id'].values[each2],
+                    ],
+                )
+
+        #inside_list.append(response3['Volumes'][each2]['Attachments'][0]['InstanceId'])
+        inside_list.append(response3['Volumes'][0]['VolumeId'])    
+        #inside_list.append(response3['Volumes'][each2]['Attachments'][0]['Device'])
+        inside_list.append(response3['Volumes'][0]['VolumeType'])
+        inside_list.append(response3['Volumes'][0]['Size'])
+        inside_list.append(response3['Volumes'][0]['Tags'][0]['Key'])
+        inside_list.append(response3['Volumes'][0]['Tags'][0]['Value'])
+        inside_list.append(response3['Volumes'][0]['State'])
+        inside_list.append(response3['Volumes'][0]['SnapshotId'])
+        
+        outside_list.append(inside_list)
+    return outside_list
+
+# Writing the CSV file
+k = list_in_lists(b)
+header = ['VolumeId', 'VolumeType', 'Size', 'TagKey', 'TagValue', 'State', 'SnapshotId']
+data1 = pd.DataFrame(k, columns=header)
+data1.to_csv(r"C:\Users\pleel\OneDrive\Downloads\samplecodes-virtusa\python-codes\volume-status.csv", index=False)
+
+# Reading the latest written file from above
+df1 = pd.read_csv(r"C:\Users\pleel\OneDrive\Downloads\samplecodes-virtusa\python-codes\volume-status.csv", usecols=['VolumeId', 'State'])
+pprint(df1)
+c = len(df1)
+
+'''
+for each3 in range(0,c):
+        response4 = client2.create_snapshot(
+            VolumeId=df1['VolumeId'].values[each3],
+            TagSpecifications=[
+                {   
+                    'ResourceType': 'snapshot', 
+                    'Tags': [
+                        {
+                            'Key': 'Env',
+                            'Value': 'Dev'
+                        },
+                    ]
+                },
+            ],            
+        )
+
+m = list_in_lists(c)
+'''
+response2 = client2.describe_snapshots(
+    Filters=[
+        {
+            'Name': 'tag:Env',
+            'Values': [
+                'Dev',
+            ]
+        },
+    ],
+)
+
+c = []
+for each in range(0,len(response2['Snapshots'])):
+    b = []
+    b.append(response2['Snapshots'][each]['SnapshotId'])
+    b.append(response2['Snapshots'][each]['StartTime'])
+    b.append(response2['Snapshots'][each]['State'])
+    b.append(response2['Snapshots'][each]['StorageTier'])
+    b.append(response2['Snapshots'][each]['VolumeId'])
+    b.append(response2['Snapshots'][each]['VolumeSize'])
+    b.append(response2['Snapshots'][each]['Tags'][0]['Key'])
+    b.append(response2['Snapshots'][each]['Tags'][0]['Value'])
+    c.append(b)
+
+#pprint(c)
+
+header = ['SnapshotId', 'StartTime', 'State', 'StorageTier', 'VolumeId', 'VolumeSize', 'TagKey', 'TagValue']
+data2 = pd.DataFrame(c, columns=header)
+data2.to_csv(r"C:\Users\pleel\OneDrive\Downloads\samplecodes-virtusa\python-codes\sanpshot-status.csv", index=False)
+
+# Reading the latest written file from above
+df2 = pd.read_csv(r"C:\Users\pleel\OneDrive\Downloads\samplecodes-virtusa\python-codes\sanpshot-status.csv", usecols=['SnapshotId', 'StartTime'])
+#pprint(df2)
+ite1 = []
+for ite in range(0,len(df2["StartTime"])):
+    a = df2["StartTime"].values[ite]
+    ite1.append(a.split(' ')[0])
+#pprint(ite1)
+
+new_column = pd.Series(ite1, name='StartTime')
+data2.update(new_column)
+data2.to_csv(r"C:\Users\pleel\OneDrive\Downloads\samplecodes-virtusa\python-codes\sanpshot-status1.csv", index=False)
+df3 = pd.read_csv(r"C:\Users\pleel\OneDrive\Downloads\samplecodes-virtusa\python-codes\sanpshot-status1.csv", usecols=['SnapshotId', 'StartTime'])
+#pprint(df3)
+
+
+ite2 = []
+for each in range(0, len(df3['StartTime'])):
+    d2 = date.today()
+    t2 = d2.strftime("%d-%m-%Y")
+    date1 = datetime.strptime("2024-04-10", "%Y-%m-%d")
+    date2 = datetime.strptime(ite1[each], "%Y-%m-%d")
+    
+    # Calculate the difference between the two dates
+    diff = relativedelta.relativedelta(date2, date1)
+    # Print the number of days between the two dates
+    print(ite2.append(diff.days))
+#pprint(ite2)
+
+df3.loc[:, "Snapshot Days Difference"] = ite2
+
+data2.insert(8,"Snapshot Days Difference", ite2, allow_duplicates=True)
+data2.to_csv(r"C:\Users\pleel\OneDrive\Downloads\samplecodes-virtusa\python-codes\sanpshot-status2.csv", index=False)
+df4 = pd.read_csv(r"C:\Users\pleel\OneDrive\Downloads\samplecodes-virtusa\python-codes\sanpshot-status2.csv", usecols=['SnapshotId', 'StartTime', 'Snapshot Days Difference'])
+#pprint(df4)
+
+
+for each in range(0,len(df4['Snapshot Days Difference'])):
+    if df4['Snapshot Days Difference'].values[each] > 5:
+        response = client2.delete_snapshot(
+            SnapshotId=df4['SnapshotId'].values[each],)
+    else:
+        continue
+pprint("Successfully deleted the snapshots of volumes")
+
